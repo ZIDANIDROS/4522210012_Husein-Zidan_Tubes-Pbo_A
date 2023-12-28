8825cf7584ca495217cc8e4f3c24e98d1b4a36d1
husein zidan <97558377+ZIDANIDROS@users.noreply.github.com>
2023-12-28 19:01:44 +0700

    Update MusicPlayerController.java

--- HuseinZidanJavaFX/src/main/java/com/example/mymusicplayer/MusicPlayerController.java
+++ HuseinZidanJavaFX/src/main/java/com/example/mymusicplayer/MusicPlayerController.java
@@ -2,38 +2,71 @@ package com.example.mymusicplayer;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.Slider;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
 
 import java.io.File;
+import java.util.List;
 
-public class MusicPlayerController {
 
+public class MusicPlayerController extends AbstractMusicPlayer{
     @FXML
     private Label statusLabel;
-
+    private Media media;
     private MediaPlayer mediaPlayer;
+    private String currentSong = "";
 
-    public void playMusic() {
-        File audioFile = new File("music/ORQUESTRA MALDITA (BRAZILIAN PHONK).mp3");
+    private int currentSongIndex = 0;
+    private List<String> playlist;
 
-        if (audioFile.exists()) {
-            Media media = new Media(audioFile.toURI().toString());
-            mediaPlayer = new MediaPlayer(media);
-
-            mediaPlayer.setOnReady(() -> {
-                statusLabel.setText("Playing: " + audioFile.getName());
-                mediaPlayer.play();
-            });
+    public void initialize() {
+        playlist = MusicLoader.loadMusicFromDirectory("music");
+        if (!playlist.isEmpty()) {
+            playCurrentSong();
         } else {
-            statusLabel.setText("File not found!");
+            setStatusLabel("No music found!");
         }
     }
+
+    @FXML
+    private Slider volumeSlider;
+
+
+
+    public void playMusic() {
+        if (mediaPlayer != null) {
+            if (mediaPlayer.getStatus() == MediaPlayer.Status.PAUSED) {
+                mediaPlayer.play();
+                setStatusLabel(currentSong);
+            } else if (mediaPlayer.getStatus() == MediaPlayer.Status.PLAYING) {
+                setStatusLabel("Already playing: " + currentSong);
+            } else {
+                mediaPlayer.play();
+                setStatusLabel(currentSong);
+            }
+        } else {
+            File audioFile = new File("music/ORQUESTRA MALDITA (BRAZILIAN PHONK).mp3");
+            if (audioFile.exists()) {
+                media = new Media(audioFile.toURI().toString());
+                mediaPlayer = new MediaPlayer(media);
+
+                mediaPlayer.setOnReady(() -> {
+                    setStatusLabel(audioFile.getName());
+                    mediaPlayer.play();
+                    currentSong = audioFile.getName();
+                });
+            } else {
+                setStatusLabel("File not found!");
+            }
+        }
+    }
+
 
     public void pauseMusic() {
         if (mediaPlayer != null && mediaPlayer.getStatus() == MediaPlayer.Status.PLAYING) {
             mediaPlayer.pause();
-            statusLabel.setText("Paused");
+            setStatusLabel("Paused");
         }
     }
 
@@ -41,6 +74,57 @@ public class MusicPlayerController {
         if (mediaPlayer != null) {
             mediaPlayer.stop();
             statusLabel.setText("Stopped");
+        }
+    }
+
+    protected void playCurrentSong() {
+        String currentSongPath = playlist.get(currentSongIndex);
+        File songFile = new File(currentSongPath);
+        String currentSongName = songFile.getName();
+
+        media = new Media(songFile.toURI().toString());
+        mediaPlayer = new MediaPlayer(media);
+
+        mediaPlayer.setOnReady(() -> {
+            setStatusLabel(currentSongName);
+            mediaPlayer.play();
+            currentSong = currentSongPath;
+        });
+    }
+
+
+    public void nextMusic() {
+        if (mediaPlayer != null && !playlist.isEmpty()) {
+            mediaPlayer.stop();
+            currentSongIndex = (currentSongIndex + 1) % playlist.size();
+            playCurrentSong();
+        }
+    }
+
+
+    public void previousMusic() {
+        if (mediaPlayer != null) {
+            mediaPlayer.stop();
+            currentSongIndex = (currentSongIndex - 1 + playlist.size()) % playlist.size();
+            playCurrentSong();
+        }
+    }
+
+    @Override
+
+
+
+    public void setVolume(double volume) {
+        if (mediaPlayer != null) {
+            mediaPlayer.setVolume(volume);
+        }
+    }
+
+
+    @Override
+    protected void setStatusLabel(String s) {
+        if (statusLabel != null) {
+            statusLabel.setText(s);
         }
     }
 }
