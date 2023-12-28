eb2135733abe11bd60f665af35120fa1033facad
husein zidan <97558377+ZIDANIDROS@users.noreply.github.com>
2023-12-28 19:01:22 +0700

    Update MusicPlayerApplication.java

--- HuseinZidanJavaFX/src/main/java/com/example/mymusicplayer/MusicPlayerApplication.java
+++ HuseinZidanJavaFX/src/main/java/com/example/mymusicplayer/MusicPlayerApplication.java
@@ -12,7 +12,7 @@ public class MusicPlayerApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        Scene scene = new Scene(fxmlLoader.load(), 520, 440);
         stage.setTitle("Music Player");
         stage.setScene(scene);
         stage.show();
